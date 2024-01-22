932b455e90d0458df0af9473e53f9126169de41e
Jordan Ballard <95947696+jordanmichb@users.noreply.github.com>
2024-01-21 19:17:17 -0800

    Add functionality for eraser and marker

--- script.js
+++ script.js
@@ -1,7 +1,36 @@
 const container = document.querySelector(".container");
 const generateBtn = document.querySelector("#generate");
+const eraserBtn = document.querySelector("#eraser");
+const markerBtn = document.querySelector("#marker");
 const gridSize = document.querySelector("#grid-size");
+//const squares = document.querySelectorAll(".grid-square");
+let marker = true;
 
+
+generateBtn.addEventListener('click', generateGrid);
+
+eraserBtn.addEventListener('click', () => {
+    if (marker) {
+        const squares = document.querySelectorAll(".grid-square");
+        marker = false;
+        squares.forEach(square => {
+            square.removeEventListener('mouseover', color)
+            square.addEventListener('mouseover', erase);
+        })
+    };
+});
+
+markerBtn.addEventListener('click', () => {
+    if (!marker) {
+        const squares = document.querySelectorAll(".grid-square");
+        marker = true;
+        console.log('marking')
+        squares.forEach(square => {
+            square.removeEventListener('mouseover', erase)
+            square.addEventListener('mouseover', color);
+        })
+    };
+});
 
 function removeAllChildNodes(parent) {
     // Use loop instead of innerHTML = '' to ensure event handlers are removed
@@ -10,6 +39,7 @@ function removeAllChildNodes(parent) {
     }
 }
 
+// Input border turns red if value is invalid, otherwise goes back to normal
 gridSize.oninput = () => {
     if (gridSize.value < 1 || gridSize.value > 100) {
         gridSize.style.border = "1px solid red";
@@ -21,15 +51,19 @@ gridSize.oninput = () => {
 
 };
 
-function getGridSize() {
-    const size = gridSize.value;
+function color() {
+    this.style.backgroundColor = 'black';
 }
 
+function erase() {
+    this.style.backgroundColor = 'white';
+}
+
+// Generate a new grid based on dimensions in input box
 function generateGrid() {
     const dimensions = gridSize.value;
     if (dimensions > 0 && dimensions < 101) {
         removeAllChildNodes(container);
-        console.log(dimensions)
         for (let i = 0; i < dimensions; i++) {
             for (let i = 0; i < dimensions; i++) {
                 const square = document.createElement('div');
@@ -37,9 +71,7 @@ function generateGrid() {
                 square.style.width = `${960 / dimensions}px`;
                 square.style.height = `${960 / dimensions}px`;
 
-                square.addEventListener('mouseover', () => {
-                    square.style.backgroundColor = 'black';
-                });
+                square.addEventListener('mouseover', color);
 
                 container.appendChild(square);
             }
@@ -48,6 +80,4 @@ function generateGrid() {
 }
 
 generateGrid();
-
-generateBtn.addEventListener('click', generateGrid);
 
